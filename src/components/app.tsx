c56cc1a7fbfabf94462124792753861df2da19b5
Prakhar <prakharrichhariya@gmail.com>
2023-11-04 04:43:47 +0530

    added tsconfig, prettier-config, favicons and eslint-config

---
+++ src/components/app.tsx
@@ -0,0 +1,12 @@
+/**
+ * @author Prakhar Richhariya <prakhar.richhariya@314ecorp.com>
+ * @description Asca
+ */
+import React from 'react';
+
+const App: React.FC = () => {
+	console.log('prakhar');
+	return <div />;
+};
+
+export default App;
